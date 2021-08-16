173c5fdf08c6817e384561e29f7bb8431a6266e3
SINDHUJARA0BALGURI <84570484+SINDHUJARA0BALGURI@users.noreply.github.com>
2021-08-16 17:37:05 +0530

    Laser instiantation is done

--- Assets/2D Galaxy Assets/Game/scripts/Player.cs
+++ Assets/2D Galaxy Assets/Game/scripts/Player.cs
@@ -7,6 +7,10 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float playerMoveSpeed; 
     private float horizontalInput,verticalInput;
+    public GameObject laserPrefab;
+    public float fireRate = 0.25f;
+    public float canFire = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +20,22 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PlayerMovement();
+        if (Input.GetKeyDown(KeyCode.Space)||Input.GetMouseButton(0))
+        {
+            if (Time.time > canFire)
+            {
+                Instantiate(laserPrefab, transform.position, Quaternion.identity);
+                canFire = Time.deltaTime * fireRate;
+            }
+           
+        }
+    }
+    private void PlayerMovement()
+    {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.right * Time.deltaTime*playerMoveSpeed*horizontalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * playerMoveSpeed * horizontalInput);
         transform.Translate(Vector3.up * Time.deltaTime * playerMoveSpeed * verticalInput);
 
 
@@ -32,15 +49,24 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(transform.position.x, -4.2f, 0);
         }
         //player boundaries for x direction
-        if (transform.position.x > 8.2)
-        {
-            transform.position = new Vector3(8.2f, transform.position.y, 0);
-        }
-        else if (transform.position.x < -8.2)
-        {
-            transform.position = new Vector3(-8.2f, transform.position.y, 0);
-        }
+        /* if (transform.position.x > 8.2)
+         {
+             transform.position = new Vector3(8.2f, transform.position.y, 0);
+         }
+         else if (transform.position.x < -8.2)
+         {
+             transform.position = new Vector3(-8.2f, transform.position.y, 0);
+         }*/
 
-
+        if (transform.position.x > 9.5f)
+        {
+            transform.position = new Vector3(-9.5f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -9.5f)
+        {
+            transform.position = new Vector3(9.5f, transform.position.y, 0);
+        }
     }
 }
+        
+
